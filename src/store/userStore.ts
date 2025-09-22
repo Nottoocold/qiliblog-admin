443fb8a1ded8145689cdae60edf5fe18ffbf47c1
qili <chao0099xx@gmail.com>
2025-09-22 23:12:21 +0800

    refactor(userStore): 重构用户状态管理逻辑，移除无关逻辑，职责单一

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,15 +1,12 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import type { UserInfo } from '@/types/login';
-import { setToken as setTokenUtil, clearToken as clearTokenUtil } from '@/utils/tokenUtils';
-import { getUserInfo, logout } from '@/services/auth.api';
 
 interface UserState {
   user: UserInfo | null;
   isAuthenticated: boolean;
-  onLogin: (accessToken: string, refreshToken: string) => Promise<void>;
-  fetchUserInfo: () => Promise<void>;
-  logout: () => Promise<void>;
+  setUserState: (user: UserInfo | null, isAuthenticated: boolean) => void;
+  clear: () => void;
 }
 
 export const useUserStore = create<UserState>()(
@@ -17,25 +14,9 @@ export const useUserStore = create<UserState>()(
     set => ({
       user: null,
       isAuthenticated: false,
-      onLogin: async (accessToken, refreshToken) => {
-        // 保存token到localStorage
-        setTokenUtil(accessToken, refreshToken);
-        // 获取用户信息
-        const userInfoResponse = await getUserInfo();
-        set({ user: userInfoResponse.data, isAuthenticated: true });
-      },
-      fetchUserInfo: async () => {
-        // 获取用户信息
-        const userInfoResponse = await getUserInfo();
-        set({ user: userInfoResponse.data, isAuthenticated: true });
-      },
-      logout: async () => {
-        await logout();
-        // 清理token
-        clearTokenUtil();
-        // 清理用户信息和认证状态
+      setUserState: (user, isAuthenticated) => set({ user, isAuthenticated }),
+      clear: () => {
         set({ user: null, isAuthenticated: false });
-        // 清理user-storeage
         localStorage.removeItem('user-storage');
       },
     }),
